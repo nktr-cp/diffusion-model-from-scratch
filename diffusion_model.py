27a4cb180bd705fd67f2b9eb361198147d7703f1
nktr-cp <nktrcp@gmail.com>
2025-05-23 12:45:49 +0900

    [feat] implemented pos_encoding

---
+++ diffusion_model.py
@@ -0,0 +1,30 @@
+import torch
+
+# Positional Encoding
+def _pos_encoding(time: int, output_dim, device='cpu'):
+	D = output_dim
+	encoded_vector = torch.zeros(D, device=device)
+
+	# [0, 1, ..., D-1]
+	i = torch.arange(0, D, device=device)
+	div_term = 10000 ** (i / D)
+
+	# i: even
+	encoded_vector[0::2] = torch.sin(time / div_term[0::2])
+	# i: odd
+	encoded_vector[1::2] = torch.cos(time / div_term[1::2])
+	
+	return encoded_vector
+
+def pos_encoding(times: torch.tensor, output_dim, device='cpu'):
+	batch_size = len(times)
+	encoded_matrix = torch.zeros(batch_size, output_dim, device=device)
+
+	for i in range(batch_size):
+		encoded_matrix[i] = _pos_encoding(times[i], output_dim, device)
+
+	return encoded_matrix
+
+if __name__ == "__main__":
+	v = pos_encoding(torch.tensor([1, 2, 3]), 16)
+	print(v.shape)
