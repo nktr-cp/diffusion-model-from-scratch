0bdbf7d8d0d6aa0d3cef23f8ec3f669ce865badb
nktr-cp <nktrcp@gmail.com>
2025-05-24 19:47:21 +0900

    [feat] add mlp and reshape procedure to ConvBlock

--- diffusion_model.py
+++ diffusion_model.py
@@ -1,4 +1,28 @@
 import torch
+from torch import nn
+
+class ConvBlock(nn.Module):
+	def __init__(self, in_ch, out_ch, time_embed_dim):
+		super().__init__()
+		self.convs = nn.Sequential(
+			nn.Conv2d(in_ch, out_ch, 3, padding=1),
+			nn.BatchNorm2d(out_ch),
+			nn.ReLU(),
+			nn.Conv2d(out_ch, out_ch, 3, padding=1),
+			nn.BatchNorm2d(out_ch),
+			nn.ReLU(),
+		)
+		self.mlp = nn.Sequencial(
+			nn.Linear(time_embed_dim, in_ch),
+			nn.ReLU(),
+			nn.Linear(in_ch, in_ch)
+		)
+	
+	def forward(self, x, v):
+		N, C, _, _ = x.shape
+		v = self.mlp(v)
+		v = v.view(N, C, 1, 1)
+		return self.convs(x + v)
 
 # Positional Encoding
 def _pos_encoding(time: int, output_dim, device='cpu'):
