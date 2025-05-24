bb5f16fc5e310e859e99737fbdfffb21ab9ad3c1
nktr-cp <nktrcp@gmail.com>
2025-05-24 19:50:34 +0900

    [feat] integrated positional encoding with UNet

--- diffusion_model.py
+++ diffusion_model.py
@@ -24,6 +24,40 @@ class ConvBlock(nn.Module):
 		v = v.view(N, C, 1, 1)
 		return self.convs(x + v)
 
+class UNet(nn.Module):
+	def __init__(self, in_ch=1, time_embed_dim=100):
+		super().__init__()
+		self.time_embed_dim = time_embed_dim
+
+		self.down1 = ConvBlock(in_ch, 64, time_embed_dim)
+		self.down2 = ConvBlock(64, 128, time_embed_dim)
+		self.bot1 = ConvBlock(128, 256, time_embed_dim)
+		self.up2 = ConvBlock(128 + 256, 128, time_embed_dim)
+		self.up1 = ConvBlock(128 + 64, 64, time_embed_dim)
+		self.out = nn.Conv2d(64, in_ch, 1)
+
+		self.maxpool = nn.MaxPool2d(2)
+		self.upsample = nn.Upsample(scale_factor=2, mode='bilinear')
+	
+	def forward(self, x, timesteps):
+		v = pos_encoding(timesteps, self.time_embed_dim, x.device)
+
+		x1 = self.down1(x, v)
+		x = self.maxpool(x1)
+		x2 = self.down2(x, v)
+		x = self.maxpool(x2)
+
+		x = self.bot1(x, v)
+
+		x = self.upsample(x)
+		x = torch.cat([x, x2], dim = 1)
+		x = self.up2(x, v)
+		x = self.upsample(x)
+		x = torch.cat([x, x1], dim = 1)
+		x = self.up1(x, v)
+		x = self.out(x)
+		return x
+
 # Positional Encoding
 def _pos_encoding(time: int, output_dim, device='cpu'):
 	D = output_dim
